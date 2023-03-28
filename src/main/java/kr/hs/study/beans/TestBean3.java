a125724b774079d1ae2e4667183cd39f7c5b6afd
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-28 15:55:37 +0900

    자동 주입

---
+++ src/main/java/kr/hs/study/beans/TestBean3.java
@@ -0,0 +1,11 @@
+package kr.hs.study.beans;
+
+public class TestBean3 {
+    private DataBean1 data1;
+    private DataBean2 data2;
+
+    public TestBean3(DataBean1 data1, DataBean2 data2) {
+        this.data1 = data1;
+        this.data2 = data2;
+    }
+}
